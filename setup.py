d973a9a07b8060ff27b48fc2b5f81a85c86f6bf0
Jared Lumpe <mjlumpe@gmail.com>
2015-09-26 16:19:44 -0700

    Empty package and basic setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+	name='pycyt',
+	version='0.0.1',
+	description='Basic python package for the analysis of flow cytometry data',
+	url='https://github.com/jlumpe/pycyt',
+	author='Jared Lumpe',
+	author_email='mjlumpe@gmail.com',
+	license='MIT',
+	packages=find_packages(),
+)
+
